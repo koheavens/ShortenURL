b1d741d46e2df74ec310cdaca6779f99a3078eb9
Chiang <tien23@gmail.com>
2023-03-08 11:28:32 +0800

    refactor : modify routes

--- routes/index.js
+++ routes/index.js
@@ -1,37 +1,7 @@
 const express = require('express')
 const router = express.Router()
-require('../config/mongoose')
-const generateShortUrl = require('../utils/generateShortUrl')
-const URL = require('../models/url')
+const home = require('./modules/home')
 
-router.get('/', (req, res) => {
-  res.render('index')
-})
-
-router.post('/', (req, res) => {
-  let shortUrl = generateShortUrl()
-  const originalUrl = req.body.url
-  if (!originalUrl) return res.redirect('/')
-
-  // 利用findOne查詢資料庫有無相同的URL，findOne 有找到資料回傳Object 找不到會回傳null
-  URL.findOne({ originalUrl })
-    .then((data) => (data ? data : URL.create({ originalUrl, shortUrl })))
-    .then((data) => {
-      res.render('index', {
-        originalUrl,
-        origin: req.headers.origin,
-        shortUrl: data.shortUrl,
-      })
-    })
-    .catch((error) => console.log(error))
-})
-
-router.get('/:shortUrl', (req, res) => {
-  const { shortUrl } = req.params
-  URL.findOne({ shortUrl }).then((data) => {
-    if (!data) return res.status(404).send('<h1>短網址不存在</h1>')
-    res.redirect(`${data.originalUrl}`)
-  })
-})
+router.use('/', home)
 
 module.exports = router
