45af4a7c6701d55b514b46a6d0a101fb720ca33a
Chiang <tien23@gmail.com>
2023-03-09 09:25:31 +0800

    feat: add check shortUrl

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -7,22 +7,32 @@ router.get('/', (req, res) => {
   res.render('index')
 })
 
-router.post('/', (req, res) => {
-  let shortUrl = generateShortUrl()
-  const originalUrl = req.body.url
-  if (!originalUrl) return res.redirect('/')
+router.post('/', async (req, res) => {
+  try {
+    let shortUrl = generateShortUrl()
+    const originalUrl = req.body.url
+    if (!originalUrl) return res.redirect('/')
 
-  // 利用findOne查詢資料庫有無相同的URL，findOne 有找到資料回傳Object 找不到會回傳null
-  URL.findOne({ originalUrl })
-    .then((data) => (data ? data : URL.create({ originalUrl, shortUrl })))
-    .then((data) => {
-      res.render('index', {
-        originalUrl,
-        origin: req.headers.origin,
-        shortUrl: data.shortUrl,
-      })
+    //check shortUrl
+    let duplicate = await URL.findOne({ shortUrl })
+    while (duplicate) {
+      shortUrl = generateShortUrl()
+      duplicate = await URL.findOne({ shortUrl })
+    }
+
+    let data = await URL.findOne({ originalUrl })
+    if (!data) {
+      data = await URL.create({ originalUrl, shortUrl })
+    }
+
+    res.render('index', {
+      originalUrl,
+      origin: req.headers.origin,
+      shortUrl: data.shortUrl,
     })
-    .catch((error) => console.log(error))
+  } catch (err) {
+    console.log(err)
+  }
 })
 
 router.get('/:shortUrl', (req, res) => {
